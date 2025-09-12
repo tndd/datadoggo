50ae405af16dfbf44d5b18fad5e015052a68dea8
tau_m2 <36781951+tndd@users.noreply.github.com>
2025-09-12 22:30:58 +0900

    rename: searchに改名

--- src/core/article/mod.rs
+++ src/core/article/mod.rs
@@ -1,11 +1,11 @@
 mod command;
 mod fetch;
 mod model;
-mod query; // 内部ユーティリティ
+mod search; // 内部ユーティリティ
 
 pub use self::command::{fetch_via_firecrawl_and_store_article_content, store_article_content};
 pub use self::fetch::fetch_article_content_via_firecrawl;
 pub use self::model::{
     Article, ArticleContent, ArticleQuery, ArticleStatus, ArticleUrlStatus, ArticleUrlStatusQuery,
 };
-pub use self::query::{search_article_url_statuses, search_articles};
+pub use self::search::{search_article_url_statuses, search_articles};
