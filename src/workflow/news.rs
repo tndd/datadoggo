a0404bcdd3344a1706b5cef2892c0390de5d015f
tau_m2 <36781951+tndd@users.noreply.github.com>
2025-09-17 00:48:44 +0900

    workflowという雛型

---
+++ src/workflow/news.rs
@@ -0,0 +1,16 @@
+/// 特定グループの記事の最新リンクを取得する
+fn update_article_links_of_group(group: &str) {
+    // TODO
+}
+
+/// 全ての記事の最新リンクを取得する
+pub fn update_article_links() {
+    // TODO
+}
+
+/// 全ての未取得リンクの記事を取得する
+pub fn fetch_articles() {
+    // TODO
+}
+
+// TODO: バックログリンクという扱い
